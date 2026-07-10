5f7e1d2180177c3e0f49595f5ab8eac3b4d0b93a
Kashika Agrawal <kashikaagrawal90@gmail.com>
2026-07-11 00:30:43 +0530

    Create Flipping an Image.cpp

---
+++ Day-31/Flipping an Image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+
+        for(auto &row : image){
+
+            int left = 0;
+            int right = row.size()-1;
+
+            while(left <= right){
+
+                int temp = row[left] ^ 1;
+                row[left] = row[right] ^ 1;
+                row[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return image;
+    }
+};
